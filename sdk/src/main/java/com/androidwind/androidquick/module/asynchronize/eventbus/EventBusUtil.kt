1a9b5714d7f8feb1853fb9065078f1ee77683e19
ddnosh <ddnosh@qq.com>
2020-02-08 17:46:55 +0800

    添加@JvmStatic

--- sdk/src/main/java/com/androidwind/androidquick/module/asynchronize/eventbus/EventBusUtil.kt
+++ sdk/src/main/java/com/androidwind/androidquick/module/asynchronize/eventbus/EventBusUtil.kt
@@ -7,18 +7,22 @@ import org.greenrobot.eventbus.EventBus
  * @website http://blog.csdn.net/ddnosh
  */
 object EventBusUtil {
+    @JvmStatic
     fun register(subscriber: Any) {
         EventBus.getDefault().register(subscriber)
     }
 
+    @JvmStatic
     fun unregister(subscriber: Any) {
         EventBus.getDefault().unregister(subscriber)
     }
 
+    @JvmStatic
     fun sendEvent(event: EventCenter<Any>?) {
         EventBus.getDefault().post(event)
     }
 
+    @JvmStatic
     fun sendStickyEvent(event: EventCenter<Any>?) {
         EventBus.getDefault().postSticky(event)
     }
